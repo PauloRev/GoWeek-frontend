fb9a339b651b8e7d1b0c1afc723b0bc244d70d00
PauloRev <paulohenriquerev@gmail.com>
2019-08-26 10:28:20 -0300

    frontend application goweek

---
+++ src/pages/Login.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+import Logo from '../assets/twitter.svg'
+
+import './Login.css'
+
+export default class Login extends Component {
+  state = {
+    username: ''
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+    const { username } = this.state
+
+    if (!username.length) return
+
+    localStorage.setItem('@GoTwitter:username', username)
+
+    this.props.history.push('/timeline')
+  }
+
+  render() {
+    return (
+      <div className="login-wrapper">
+        <img src={Logo} alt="Logo" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Nome de usuário"
+            value={this.state.username}
+            onChange={this.handleInputChange}
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    )
+  }
+}
